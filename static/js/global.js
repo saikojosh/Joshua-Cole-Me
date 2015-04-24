9cf45a2788aab73ebca5711225d7b51188ccb629
Josh Cole <saikojosh@gmail.com>
2015-04-24 22:33:02 +0100

    Fix: scrolling div vanishes on CSS 3d transform

--- static/js/global.js
+++ static/js/global.js
@@ -7,7 +7,9 @@ var Global = {
    * Global properties.
    */
   prop: {
-
+    $mainCard:            null,
+    $aboutText:           null,
+    aboutTextScrollFixed: false
   },
 
   /*
@@ -16,7 +18,8 @@ var Global = {
   setup: function () {
 
     // Cache selectors.
-    Global.prop.$mainCard = $('#main > .card');
+    Global.prop.$mainCard  = $('#main > .card');
+    Global.prop.$aboutText = $('#about > .inner > .text');
 
   },
 
@@ -24,7 +27,18 @@ var Global = {
    * Flips the card to show the about side.
    */
   showAbout: function () {
+
+    // Show the card first.
     Global.prop.$mainCard.addClass('flipped');
+
+    // Fix: Only allow the about text element to scroll after the CSS animation is over.
+    if (!Global.prop.aboutTextScrollFixed) {
+      setTimeout(function () {
+        Global.prop.$aboutText.css('overflow-y', 'auto');
+        Global.prop.aboutTextScrollFixed = true;
+      }, (1000 * 1.6));
+    }
+
   },
 
   /*
